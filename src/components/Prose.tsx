99f69573452a1b42eedf57a56fa07e7d14441d07
Antonio C. Monroy <tony_monroe1212@hotmail.com>
2026-06-23 23:04:32 -0400

    fix some spacing things

--- src/components/Prose.tsx
+++ src/components/Prose.tsx
@@ -7,7 +7,7 @@ interface Props {
 const Prose: FC<Props> = ({children}) => {
 	return (
 		<div
-			className="prose prose-li:text-fg max-w-none prose-h1:text-orange prose-p:text-fg prose-h2:text-blue prose-h3:text-fg prose-li:marker:text-fg prose-hr:border-border prose-a:text-accent prose-strong:text-fg"
+			className="text-lg prose prose-li:text-fg max-w-none prose-h1:text-orange prose-p:text-fg prose-h2:text-blue prose-h3:text-fg prose-li:marker:text-fg prose-hr:border-border prose-a:text-accent prose-strong:text-fg"
 		>
 			{children}
 		</div>
